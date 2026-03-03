7ea6774c69a52ddd4cbbb1d9bd23005ca096efa1
ketjonam <ketjona.mema@kreatx.com>
2026-03-03 13:42:48 +0100

    MUN-2428 Routes Assignement Test Automation

---
+++ Routes Management/Routes Assigned/RoutesAssgn.cs
@@ -0,0 +1,56 @@
+﻿using Mobiread.Test;
+using OpenQA.Selenium;
+using System.Threading;
+
+namespace Mobiread.RoutesMangement.RoutesAssigned
+{
+    class RoutesAssgn : BasePage
+    {
+        public RoutesAssgn(IWebDriver driver) : base(driver) { }
+
+        public IWebElement Menu_RouteManagement =>
+            driver.FindElement(By.XPath("/html/body/div/div/div[1]/div/div/div/div[2]/ul/div[2]/div[2]/span"));
+
+        public IWebElement SubMenu_AssignRoute =>
+            driver.FindElement(By.XPath("/html/body/div/div/div[1]/div/div/div/div[2]/ul/div[3]/div/div/div/div[2]/div[2]/span"));
+
+        public IWebElement ChooseReader =>
+            driver.FindElement(By.XPath("/html/body/div/div/div[2]/div[2]/div/div/div[1]/div[2]/div[1]/div[2]/p"));
+        public IWebElement SelectDate =>
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[3]/div/div[9]/div[1]/span[1]"));
+        public void RoutesAssign()
+        {
+            Thread.Sleep(1000);
+            Menu_RouteManagement.Click();
+
+            Thread.Sleep(500);
+            SubMenu_AssignRoute.Click();
+
+            Thread.Sleep(500);
+            ChooseReader.Click();
+
+            Thread.Sleep(500);
+            SelectDate.Click();
+        }
+
+        public void UnassignedRoutes()
+        {
+            IWebElement unassignedrouteselect = driver.FindElement(By.XPath(
+                  "/html/body/div[2]/div[3]/div/div[1]/div[2]/div[1]/div/div/div[3]/div[1]/p"
+              ));
+            unassignedrouteselect.Click();
+            
+            IWebElement option2 = driver.FindElement(By.XPath(
+                "/html/body/div[2]/div[3]/div/div[1]/div[2]/div[1]/div/div/div[3]/div[2]/p"
+            ));
+            option2.Click();
+
+            IWebElement Button = driver.FindElement(By.XPath("/html/body/div[2]/div[3]/div/div[1]/div[2]/div[2]/div/div/span[1]/button"));
+            Button.Click();
+
+            IWebElement SaveButton = driver.FindElement(By.XPath("/html/body/div[2]/div[3]/div/div[2]/button[2]"));
+            SaveButton.Click();
+
+        }
+    }
+}
---
+++ Routes Management/Routes Assigned/RoutesAssgnTest.cs
@@ -0,0 +1,34 @@
+﻿using Mobiread.Test.Login;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Threading;
+using Mobiread.RoutesMangement.RoutesAssigned;
+
+namespace Mobiread.Test.RoutesAssgnTests
+{
+    [TestFixture]
+    public class RoutesAssgnTest : BasePage
+    {
+        [Test]
+        public void PerformLogin_then_RoutesAssign()
+        {
+            Console.WriteLine("Test started...");
+
+            // 1) Login
+            LoginPage login = new LoginPage(driver);
+            login.PerformLogin();
+
+            Thread.Sleep(3000);
+            //Caktimi i shtigjeve
+            RoutesAssgn view = new RoutesAssgn(driver);
+            view.RoutesAssign();
+
+            Thread.Sleep(500);
+            view.UnassignedRoutes();
+
+            Console.WriteLine("Export completed.");
+        }
+    }
+}
+
