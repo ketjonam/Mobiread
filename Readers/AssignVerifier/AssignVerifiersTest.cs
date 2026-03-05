f774a82c30a59fb46aff370f0de442085e8bbfc4
ketjonam <ketjona.mema@kreatx.com>
2026-03-05 11:01:40 +0100

    MUN-2443 Test Automation Readers

---
+++ Readers/AssignVerifier/AssignVerifiersTest.cs
@@ -0,0 +1,37 @@
+﻿using Mobiread.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mobiread.Readers.AssignVerifiers;
+using Mobiread.Test.Login;
+
+namespace Mobiread.Readers.AssignVerifiersTests
+{
+    [TestFixture]
+    public class AssignVerifiersTest : BasePage
+    {
+        [Test]
+        public void PerformLogin_then_VerifierAssign()
+        {
+            Console.WriteLine("Test started...");
+            // 1) Login
+            LoginPage login = new LoginPage(driver);
+            login.PerformLogin();
+            Thread.Sleep(3000);
+            //Caktimi i verifikuesve
+            Mobiread.Readers.AssignVerifiers.AssignVerifiers view = new Mobiread.Readers.AssignVerifiers.AssignVerifiers(driver);
+            view.VerifierAssign();
+            Thread.Sleep(2000);
+            view.SelectVerifier("verifikues.kreatx1");
+            Thread.Sleep(2000);
+            view.AllReaders();
+            Thread.Sleep(2000);
+            view.Button();
+            Thread.Sleep(500);
+            view.AssignedReaders();
+            Console.WriteLine("Export completed.");
+        }
+    }
+}
